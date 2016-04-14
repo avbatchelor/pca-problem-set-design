b632b7611c300b3535a32297cfc1c18a63f072a3
avbatchelor <alexbatchelor@hotmail.co.uk>
2016-04-14 14:51:25 -0400

    Alex's PCA code added

---
+++ parseData.m
@@ -0,0 +1,20 @@
+%% Load unparsed data 
+load('C:\Users\Alex\Documents\GitHub\nb204-LNLNP-tuning-simulation\blind_neurons.mat')
+
+%% Make data matrix 
+allN_cell = struct2cell(allN);
+allN_mat = cell2mat(allN_cell);
+allN_mat_p = permute(allN_mat,[2 1 3]);
+N_mat = reshape(allN_mat_p,6000,58);
+N_mat = N_mat';
+
+data = N_mat;
+
+all_stim = reshape(stim',[],1);
+stim_down = 5*downsample(all_stim,5);
+samprate = 1/1000;
+time = samprate:samprate:6;
+stim = stim_down; 
+stim = stim';
+
+save('C:\Users\Alex\Documents\GitHub\nb204-LNLNP-tuning-simulation\pca_data.mat','data','stim','time')
