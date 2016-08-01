fe143705fb7af2d9613865bf5a2c89d712c7f15f
avbatchelor <alexbatchelor@hotmail.co.uk>
2016-08-01 18:08:42 -0400

    alex laptop sync

---
+++ avb homework version 1/old_my_save_fig.m
@@ -0,0 +1,16 @@
+function my_save_fig(filename)
+
+% Make the figure full screen 
+p = get(0,'MonitorPositions');
+set(gcf,'Position',p(1,:)) % Makes the figure full screen on the first monitor
+
+% Set figure size 
+if ~exist('figSize','var')
+    figSize = [5 3];
+end
+% set(gcf, 'PaperSize', figSize);
+% set(gcf,'PaperUnits','inches','PaperPositionMode','manual','PaperPosition',[0 0,figSize]);
+
+% Save as emf file 
+imageFilename = [filename,'.pdf'];
+saveas(gcf,filename,'pdf')
